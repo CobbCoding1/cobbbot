3e3b621d0c3f12ed1704de8e8a26b9de6c7da133
CobbCoding <rircobb@gmail.com>
2023-11-18 15:06:57 -0500

    removed third-party dependency

--- main.go
+++ main.go
@@ -2,24 +2,29 @@ package main
 
 import (
     "fmt"
+    "github.com/CobbCoding1/goirc"
     "os"
     "log"
     "strings"
     "time"
-    "github.com/gempir/go-twitch-irc"
 )
 
 const (
-    todayfilename = "todaydata"
+    domain = "irc.twitch.tv"
+    port = "6667"
+    server = "cobbcoding"
+    name = "cobbbot"
+    todayfilename = "/.config/cobbbot/todaydata"
+    discord = "https://discord.gg/3SkpwrRxpA"
 )
 
-func handle_error(err error){
+func handle_error(err error) {
     if err != nil {
         log.Fatal(err)
     }
 }
 
-func read_token_from_file(filename string)(string) {
+func read_string_from_file(filename string)(string) {
     data, err := os.ReadFile(filename)
 
     handle_error(err)
@@ -36,81 +41,93 @@ func write_data_to_file(filename string, data string) {
     file.WriteString(data)
 }
 
-func main(){
-    access_token := read_token_from_file(".client")
-    client := twitch.NewClient("cobbcoding", access_token)
+
+func main() {
+    home_dir, home_err := os.UserHomeDir()
+    handle_error(home_err)
 
     commands := map[string]string{
-        "!help": "List all possible commands",
-        "!ping": "PONG",
-        "!today": "List the current project for today",
-        "!socials": "List all my social media accounts",
-        "!yt": "Get URL for my YouTube channel",
-        "!surprise": "Run for a fun surpise",
-        "!specs": "List PC specs and OS information",
-        "!say": "Make the bot say something",
-        "!time": "List the current time in the streamers' area",
-        "!sub": "to subscribe",
+        "help": "List all possible commands",
+        "ping": "PONG",
+        "today": "List the current project for today",
+        "socials": "List all my social media accounts",
+        "yt": "Get URL for my YouTube channel",
+        "surprise": "Run for a fun surpise",
+        "specs": "List PC specs and OS information",
+        "say": "Make the bot say something",
+        "time": "List the current time in the streamers' area",
+        "sub": "to subscribe",
+        "69": "nice",
+        "nice": "69",
+        "discord": "discord",
     }
 
-    client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-        if(message.Message[0] == '!') {
-            msg := message.Message[1:]
-            stripped_msg := strings.Fields(msg)
-            switch(stripped_msg[0]){
+    var client goirc.Client
+    token := read_string_from_file(".client")
+    client.Init(domain, port, token, "", name) 
+    client.Connect()
+    defer client.Disconnect()
+
+    client.Join(server)
+    for {
+        msg := client.GetData()
+        if msg.IsPrivateMessage() {
+            if(msg.Message[0] == '!') {
+                command := msg.Message[1:]
+                stripped_command := strings.Fields(command)
+                fmt.Println(stripped_command)
+                switch(stripped_command[0]){
                 case "ping":
-                    client.Say(message.Channel, "Pong!")
+                    client.Say("Pong!")
                 case "settoday":
-                    if(message.User.Name != "cobbcoding") {
-                        client.Say(message.Channel, "You're not allowed to do that")
+                    if(msg.Username != "cobbcoding") {
+                        client.Say("You're not allowed to do that")
                     } else {
-                        client.Say(message.Channel, "Set the topic of today's stream successfully")
-                        write_data_to_file(todayfilename, strings.Join(stripped_msg[1:], " "))
+                        client.Say("Set the topic of today's stream successfully")
+                        write_data_to_file(home_dir + todayfilename, strings.Join(stripped_command[1:], " "))
                     }
                 case "shoutout":
-                    if(message.User.Name != "cobbcoding") {
-                        client.Say(message.Channel, "You're not allowed to do that")
+                    if(msg.Username != "cobbcoding") {
+                        client.Say("You're not allowed to do that")
                     } else {
-                        streamer := strings.Join(stripped_msg[1:], " ")
-                        client.Say(message.Channel, fmt.Sprintf("Go follow %s on Twitch! twitch.tv/%s", streamer, streamer))
+                        streamer := strings.Join(stripped_command[1:], " ")
+                        client.Say(fmt.Sprintf("Go follow %s on Twitch! twitch.tv/%s", streamer, streamer))
                     }
                 case "today":
-                    client.Say(message.Channel, read_token_from_file(todayfilename))
+                    client.Say(read_string_from_file(home_dir + todayfilename))
                 case "socials":
-                    client.Say(message.Channel, "X (Twitter): https://x.com/cobbcoding, YouTube: https://youtube.com/@cobbcoding, GitHub: https://github.com/CobbCoding1")
+                    client.Say("X (Twitter): https://x.com/cobbcoding, YouTube: https://youtube.com/@cobbcoding, GitHub: https://github.com/CobbCoding1")
                 case "yt":
-                    client.Say(message.Channel, "https://youtube.com/@cobbcoding")
+                    client.Say("https://youtube.com/@cobbcoding")
                 case "specs":
-                    client.Say(message.Channel, "specs: i5 2400, 12GB RAM, Radeon R7 260X. Arch Linux BTW")
+                    client.Say("specs: i5 2400, 12GB RAM, Radeon R7 260X. Arch Linux BTW")
                 case "surprise":
-                    client.Say(message.Channel, "Enjoy your surpise: https://www.youtube.com/watch?v=xvFZjo5PgG0")
+                    client.Say("Enjoy your surpise: https://www.youtube.com/watch?v=xvFZjo5PgG0")
                 case "say":
-                    client.Say(message.Channel, strings.Join(stripped_msg[1:], " "))
+                    client.Say(strings.Join(stripped_command[1:], " "))
                 case "time":
-                    client.Say(message.Channel, time.Now().Format(time.RFC850))
+                    client.Say(time.Now().Format(time.RFC850))
                 case "sub":
-                    client.Say(message.Channel, "https://www.twitch.tv/subs/cobbcoding")
+                    client.Say("https://www.twitch.tv/subs/cobbcoding")
+                case "69":
+                    client.Say("nice")
+                case "nice":
+                    client.Say("69")
+                case "discord":
+                    client.Say("The Discord is: " + discord)
                 case "help":
                     output := ""
                     for key, data := range commands {
                         output += key + ": "
-                        output += data + ",     "
+                        output += data + " --- "
                     }
-                    client.Say(message.Channel, output)
+                    client.Say(output)
                 default:
-                    client.Say(message.Channel, "unknown command: !" + msg)
+                    client.Say("unknown command: !" + command)
+                }
             }
-        } 
-    })
+        }
 
-    client.OnUserJoinMessage(func(message twitch.UserJoinMessage) {
-        client.Say(message.Channel, fmt.Sprintf("%s: has joined the chat", message.User))
-    })
-
-    client.Join("cobbcoding")
-
-    err := client.Connect()
-
-    handle_error(err)
+        client.HandlePong()
+    }
 }
-
